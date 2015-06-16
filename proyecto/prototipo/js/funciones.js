975ec0980537b70e1c94c4fe5c85ac948899d122
yordanch <snd.yvv@gmail.com>
2015-06-15 23:10:56 -0500

    drawing v1.1

--- proyecto/prototipo/js/funciones.js
+++ proyecto/prototipo/js/funciones.js
@@ -1,1 +1,19 @@
-alert("hola mundo")
+window.onload=inicio;
+function inicio() {
+	dibujarCanvas();
+	dubujoCamenzar();
+}
+function dibujarCanvas(){
+	var canvas=document.createElement('canvas');
+	var campo=document.getElementById('canvasImprim');
+	canvas.id="dibujito";
+	campo.appendChild(canvas);
+}
+function dubujoCamenzar(){
+	canvas=document.getElementById("dibujito");
+	if(canvas.getContext){
+		contexto=canvas.getContext("2d");
+	}else{
+		alert("Disculpe por las molestias, pero no puedes dibujar, necesitas el navegador actualizado");
+	}
+}
