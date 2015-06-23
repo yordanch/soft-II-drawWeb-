efaf5e39cc963f0cf5cc776163aeeb3c47ed74c2
yordanch <snd.yvv@gmail.com>
2015-06-23 18:01:49 -0500

    .......

--- proyecto/prototipo/js/funciones.js
+++ proyecto/prototipo/js/funciones.js
@@ -2,6 +2,8 @@ window.onload=inicio;
 function inicio() {
 	dibujarCanvas();
 	dubujoCamenzar();
+	Tamanio();
+	window.onresize=Tamanio;
 }
 function dibujarCanvas(){
 	var canvas=document.createElement('canvas');
@@ -13,7 +15,16 @@ function dubujoCamenzar(){
 	canvas=document.getElementById("dibujito");
 	if(canvas.getContext){
 		contexto=canvas.getContext("2d");
+		contexto.strokeRect(20,20,20,20);
+		contexto.stroke();
 	}else{
 		alert("Disculpe por las molestias, pero no puedes dibujar, necesitas el navegador actualizado");
 	}
+}
+function Tamanio(){
+	var mat=document.getElementById('materiales');
+	var can=document.getElementById('canvasImprim');
+	var CampoDibujo=document.getElementById('campoDraw');
+	canvas.width=CampoDibujo.offsetWidth-mat.offsetWidth-30;
+	console.log(CampoDibujo.offsetWidth-mat.offsetWidth);
 }
